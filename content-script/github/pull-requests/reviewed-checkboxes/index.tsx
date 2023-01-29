fb5999ef2ffad5fba3da34e872fe751c8893640b
Emmanuel <pastor1212@gmail.com>
2023-01-29 16:25:42 +0100

    feat(content/github): Prepare code that should remove old saved checked PR

--- content-script/github/pull-requests/reviewed-checkboxes/index.tsx
+++ content-script/github/pull-requests/reviewed-checkboxes/index.tsx
@@ -2,6 +2,7 @@ import { createSignal } from "solid-js";
 import browser from "webextension-polyfill";
 import { ColumnCheckbox } from "./ColumnCheckbox";
 import { ColumnTitle } from "./ColumnTitle";
+import difference from "lodash.difference";
 
 type TReviewedPrStorage = {
   [organization: string]: {
@@ -16,10 +17,11 @@ type TTurboLoadEventDetail = {
 };
 
 function onTurboLoad(event: CustomEvent<TTurboLoadEventDetail>) {
-  if (event.detail.url.includes("/pull")) {
+  const url = new URL(event.detail.url);
+  if (url.pathname.includes("/pull")) {
     const prCheckbox = document.querySelector("[data-eqx-checkbox]");
     if (!prCheckbox) {
-      AddCheckboxesElements();
+      AddCheckboxesElements(url);
     }
   }
 }
@@ -30,13 +32,15 @@ window.addEventListener("turbo:load", onTurboLoad);
 // TODO: At some point remove the old PR from the storage
 export const [reviewedPrs, setReviewedPrs] = createSignal<TReviewedPrStorage>({});
 
-export async function AddCheckboxesElements() {
-  const [_ignore, organization, repository] = window.location.pathname.split("/");
+export async function AddCheckboxesElements(url: URL) {
+  const [_ignore, organization, repository] = url.pathname.split("/");
+  const search = url.search;
   const prLines = document.querySelectorAll("div[id^=issue_]");
   const colTitle = document.querySelector("[data-eqx-col-title]");
 
   if (prLines.length > 0) {
-    await initReviewedPullrequestStorage(organization, repository);
+    const allIssuesIds = Array.from(prLines).map((prLine) => prLine.id);
+    await initReviewedPullrequestStorage(organization, repository, allIssuesIds, search);
     if (!colTitle) {
       const titleRowParent = document.querySelector(".table-list-filters");
       titleRowParent?.appendChild(<ColumnTitle /> as Node);
@@ -57,10 +61,20 @@ export async function AddCheckboxesElements() {
   }
 }
 
-async function initReviewedPullrequestStorage(organization: string, repository: string) {
+async function initReviewedPullrequestStorage(organization: string, repository: string, allIssuesIds: string[], search: string) {
   const storageResponse = await browser.storage.local.get("reviewedPrs");
   if (storageResponse.reviewedPrs) {
     setReviewedPrs(storageResponse.reviewedPrs);
+
+    // To update and try later on
+    // if (!search && storageResponse.reviewedPrs[organization][repository]) {
+    //   const allSavedIssuesIds = Object.keys(storageResponse.reviewedPrs[organization][repository]);
+    //   allSavedIssuesIds.push("issue_1");
+    //   const issuesToRemove = difference(allSavedIssuesIds, allIssuesIds);
+    //   issuesToRemove.forEach((issue: string) => {
+    //     delete storageResponse.reviewedPrs[organization][repository][issue];
+    //   });
+    // }
   } else {
     browser.storage.local.set({ reviewedPrs:{ [organization]: { [repository]: {} } } });
   }

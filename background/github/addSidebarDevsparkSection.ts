a1c98d5e7714c03780e7af2501379fc154e5f759
Emmanuel <pastor1212@gmail.com>
2023-05-18 18:56:50 +0200

    feat(background/github): Make the devspark sidebar sticky

--- background/github/addSidebarDevsparkSection.ts
+++ background/github/addSidebarDevsparkSection.ts
@@ -23,7 +23,7 @@ browser.tabs.onActivated.addListener(async () => {
 });
 
 function addDevsparkSectionToConversationPage() {
-  const discussionSidebarContainer = document.querySelector("#partial-discussion-sidebar");
+  const discussionSidebarContainer = document.querySelector<HTMLDivElement>("#partial-discussion-sidebar");
   const firstDiscussionSideBarItem = document.querySelector(".discussion-sidebar-item.sidebar-assignee.js-discussion-sidebar-item");
   const devsparkSectionExist = document.querySelector("[data-devspark-section-inserted");
 
@@ -32,11 +32,16 @@ function addDevsparkSectionToConversationPage() {
     devsparkSidebarSection.id = "devspark-sidebar-section";
     devsparkSidebarSection.classList.add("discussion-sidebar-item", "sidebar-assignee", "js-discussion-sidebar-item", "text-bold", "discussion-sidebar-heading");
     devsparkSidebarSection.dataset.devsparkSectionInserted = "true";
+    devsparkSidebarSection.style.position = "sticky";
+    devsparkSidebarSection.style.top = "60px";
+    devsparkSidebarSection.style.zIndex = "10";
+    devsparkSidebarSection.style.backgroundColor = "var(--color-canvas-default)";
 
     const sectionTitle = document.createElement("div");
     sectionTitle.innerText = "Devspark";
     sectionTitle.classList.add("mb-2");
 
+    discussionSidebarContainer.style.height = "100%";
     devsparkSidebarSection.appendChild(sectionTitle);
 
     discussionSidebarContainer.insertBefore(devsparkSidebarSection, firstDiscussionSideBarItem);

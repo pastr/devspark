96e02b50b6d107ed6f68ac82da07057e47a74f24
Emmanuel <pastor1212@gmail.com>
2023-05-24 21:59:08 +0200

    fix(content/github): Fix devspark sidebar section dissapearing after editing PR description

--- background/github/interceptors.ts
+++ background/github/interceptors.ts
@@ -4,14 +4,15 @@ import { GITHUB_OBSERVED_URLS } from "../../_shared/enums/GITHUB_OBSERVED_URLS";
 
 browser.webRequest.onCompleted.addListener(
   async (req) => {
-    if (GITHUB_OBSERVED_URLS.PullRequestReviewDecisions.test(req.url)) {
-      const message: IMessage = {
-        eventType: "WebRequestCompleted",
-        eventDetails: {
-          url: req.url
-        }
-      };
+    const message: IMessage = {
+      eventType: "WebRequestCompleted",
+      eventDetails: {
+        url: req.url
+      }
+    };
 
+    if (GITHUB_OBSERVED_URLS.PullRequestReviewDecisions.test(req.url) ||
+        GITHUB_OBSERVED_URLS.ShowPartial.test(req.url)) {
       browser.tabs.sendMessage(req.tabId, message);
     }
   },

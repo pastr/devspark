af0930f7acdf0c71c75f44ede004936f806721db
Emmanuel <pastor1212@gmail.com>
2023-08-12 17:25:02 +0200

    refactor(all): Fix the dev scripts

--- selectManifest.js
+++ selectManifest.js
@@ -1,16 +1,23 @@
-import fs from "fs";
+import fs from "fs/promises";
+import path from 'path';
 
 const manifestV2 = "./manifests/manifestV2.json";
 const manifestV3 = "./manifests/manifestV3.json";
 const selectedManifest = process.env.CHROMIUM ? manifestV3 : manifestV2;
 const newManifest = "./dist/manifest.json";
 
-// Read the contents of selectedManifest
-fs.readFile(selectedManifest, "utf8", (err, data) => {
-  if (err) throw err;
+async function main() {
+  try {
+    const data = await fs.readFile(selectedManifest, 'utf8');
 
-  // Write the contents of selectedManifest to manifest.json
-  fs.writeFile(newManifest, data, "utf8", (err) => {
-    if (err) throw err;
-  });
-});
+    const directory = path.dirname(newManifest);
+    await fs.mkdir(directory, { recursive: true });
+
+    await fs.writeFile(newManifest, data, 'utf8');
+    console.log('Manifest created successfully');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();

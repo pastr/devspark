b7fe1212f79eed1d489fb8684e72487547442f57
Emmanuel <pastor1212@gmail.com>
2023-08-13 17:06:14 +0200

    refactor(all): fix packages version

--- apps/content-script/.eslintrc.js
+++ apps/content-script/.eslintrc.js
@@ -1,12 +1,7 @@
 /* eslint-env node */
 
 module.exports = {
-  parser: "@typescript-eslint/parser",
   extends: [
-    "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
     "devspark/common"
   ],
   rules: {}

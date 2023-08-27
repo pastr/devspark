01194391cf99d90ac7c7db21ba0a38e912a4c724
Emmanuel <pastor1212@gmail.com>
2023-08-27 22:47:59 +0200

    feat(options): Split vendors into its own chunks when building options

--- apps/options/vite.options.config.ts
+++ apps/options/vite.options.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   build: {
     outDir: "../../dist",
     emptyOutDir: false,
